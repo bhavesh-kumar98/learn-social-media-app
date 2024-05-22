533c4cd6a271aa6ed6ceb9ae1a9a2adfb0b1cfbf
Bhavesh Kumar <bhaveshsoni19559@gmail.com>
2024-05-22 17:54:03 +0530

    add toggle functionality on the sidebar comp.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
-import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 import Sidebar from "./components/Sidebar";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import CreatePost from "./components/CreatePost";
 import PostList from "./components/PostList";
+import { useState } from "react";
 
 function App() {
+  const [selectedTab, setSelectedTab] = useState("Home");
+
   return (
     <>
       <div className="app-container">
-        <Sidebar />
+        <Sidebar selectedTab={selectedTab} setSelectedTab={setSelectedTab}/>
         <div className="content">
           <Header />
-          <CreatePost></CreatePost>
-          <PostList></PostList>
+          {selectedTab === "Home" ? (
+            <CreatePost></CreatePost>
+          ) : (
+            <PostList></PostList>
+          )}
+
           <Footer />
         </div>
       </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-function Sidebar() {
+function Sidebar({ selectedTab, setSelectedTab }) {
   return (
     <div
       className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark sidebar"
@@ -15,16 +15,36 @@ function Sidebar() {
       </a>
       <hr />
       <ul className="nav nav-pills flex-column mb-auto">
-        <li className="nav-item">
-          <a href="#" className="nav-link active" aria-current="page">
+        <li
+          className="nav-item"
+          onClick={() => {
+            setSelectedTab("Home");
+          }}
+        >
+          <a
+            href="#"
+            className={`nav-link text-white ${
+              selectedTab === "Home" && "active"
+            }`}
+            aria-current="page"
+          >
             <svg className="bi pe-none me-2" width="16" height="16">
               <use xlinkHref="#home"></use>
             </svg>
             Home
           </a>
         </li>
-        <li>
-          <a href="#" className="nav-link text-white">
+        <li
+          onClick={() => {
+            setSelectedTab("CreatePost");
+          }}
+        >
+          <a
+            href="#"
+            className={`nav-link text-white ${
+              selectedTab === "CreatePost" && "active"
+            }`}
+          >
             <svg className="bi pe-none me-2" width="16" height="16">
               <use xlinkHref="#people-circle"></use>
             </svg>
